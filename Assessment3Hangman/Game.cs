62adb2014f064e7f9ee17410e1ae450dc2f80d52
Funce <funce.973@gmail.com>
2018-08-19 11:31:07 +1200

    Make hangman loop work, awaiting images for strike counter

--- Assessment3Hangman/Game.cs
+++ Assessment3Hangman/Game.cs
@@ -7,20 +7,178 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.Annotation;
 using Android.Views;
 using Android.Widget;
+using static Android.App.ActionBar;
 
 namespace Assessment3Hangman
 {
     [Activity(Label = "Game")]
     public class Game : Activity
     {
+        protected string WordToGuess;
+        protected List<char> guessedLetters;
+        protected int strikes;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Game);
 
-            // Create your application here
+            strikes = 0;
+            guessedLetters = new List<char>();
+
+            create_word();
+            FindViewById<Button>(Resource.Id.btnback).Click += btnback_Click; ;
+
+            char[] row1 = "qwertyuiop".ToCharArray();
+            char[] row2 = "asdfghjkl".ToCharArray();
+            char[] row3 = "zxcvbnm".ToCharArray();
+            char[][] rows = { row1, row2, row3 };
+            LinearLayout keyboard1 = FindViewById<LinearLayout>(Resource.Id.linKeyboard1);
+            LinearLayout keyboard2 = FindViewById<LinearLayout>(Resource.Id.linKeyboard2);
+            LinearLayout keyboard3 = FindViewById<LinearLayout>(Resource.Id.linKeyboard3);
+            LinearLayout[] keyboards = { keyboard1, keyboard2, keyboard3 };
+
+            for (int row = 0; row < rows.Length; row++)
+            {
+                for (int letter = 0; letter < rows[row].Length; letter++)
+                {
+                    var newButton = new Button(this);
+                    newButton.Click += onLetterPress;
+                    newButton.Text = rows[row][letter].ToString();
+                    newButton.SetBackgroundColor(new Android.Graphics.Color(255, 225, 30));
+                    //newButton.SetForegroundGravity(GravityFlags.Center);
+                    //newButton.SetPadding(50, 5, 50, 5);
+                    var layoutParams = new LayoutParams(80, ViewGroup.LayoutParams.MatchParent);
+                    layoutParams.SetMargins(10, 10, 10, 10);
+                    keyboards[row].AddView(newButton, layoutParams);
+                }
+            }
             
+        }
+
+        private void btnback_Click(object sender, EventArgs e)
+        {
+            base.OnBackPressed();
+        }
+
+        private void onLetterPress(object sender, EventArgs e)
+        {
+            Button pressedButton = (Button)sender;
+            string letter = pressedButton.Text;
+            pressedButton.Clickable = false;
+            pressedButton.SetBackgroundColor(new Android.Graphics.Color(180, 180, 180));
+            guess_letter(letter);
+        }
+
+        protected void guess_letter(string letter)
+        {
+            //Lets cheat the type casting
+            char guessedLetter = letter[0];
+            bool found = false;
+            var foundIndexes = new List<int>();
+            for (int i = WordToGuess.IndexOf(guessedLetter); i > -1; i = WordToGuess.IndexOf(guessedLetter, i + 1))
+            {
+                found = true;
+                // for loop end when i=-1 (guessedLetter not found)
+                guessedLetters[i] = guessedLetter;
+            }
+
+            if (!found)
+            {
+                ++strikes;
+            }
+            //Check win conditions
+            //Loss first
+            /*
+             *       |      | |
+             *       | |    | _
+             */
+            //Nyehehe
+            if (strikes == 7)
+            {
+                lose();
+            }
+            //THen the win conditions
+            else if(guessedLetters.IndexOf('_') == -1)
+            {
+                win();
+            }
+
+            update_result();
+
+        }
+
+        protected void create_word()
+        {
+            WordToGuess = get_word();
+            
+            for (int i = 0; i < WordToGuess.Length; i++)
+            {
+                if (WordToGuess[i] == ' ')
+                {
+                    guessedLetters.Add('\n');
+                }
+                else
+                {
+                    guessedLetters.Add('_');
+                }
+                
+            }
+            Toast.MakeText(this, WordToGuess, ToastLength.Long).Show();
+
+            update_result();
+            
+
+        }
+
+        protected string get_word()
+        {
+            //Read in a txt file
+            string word = "shipping container";
+            return word;
+        }
+
+        protected void update_result()
+        {
+            var sb = new StringBuilder();
+            foreach (var c in guessedLetters)
+            {
+                sb.Append(c);
+            }
+            //Change imageswitch to image[strikes]
+            FindViewById<TextView>(Resource.Id.Strikes).Text = strikes.ToString();
+            FindViewById<TextView>(Resource.Id.guessedLetters).Text = sb.ToString().ToUpper();
+        }
+
+        protected void win()
+        {
+            var dialog = new AlertDialog.Builder(this);
+            AlertDialog alert = dialog.Create();
+            alert.SetTitle("Victory!");
+            alert.SetMessage("Shipment Delivered!\n\nPlay again?");
+            alert.SetButton("Yes", (c, ev) =>
+            {
+                Finish();
+                var intent = new Intent(this, typeof(Game))
+                    .SetFlags(ActivityFlags.ReorderToFront);
+                StartActivity(intent);
+            });
+            alert.SetButton2("Quit", (c, ev) => { });
+            alert.Show();
+        }
+
+        protected void lose()
+        {
+            Android.App.AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            AlertDialog alert = dialog.Create();
+            alert.SetTitle("Defeat");
+            alert.SetMessage("You lost all the containers!");
+            alert.SetButton("Restart", (c, ev) => { });
+            alert.SetButton("Quit", (c, ev) => { });
+            alert.Show();
         }
     }
 }
--- Assessment3Hangman/MainActivity.cs
+++ Assessment3Hangman/MainActivity.cs
@@ -7,18 +7,21 @@ using Android.Support.V7.App;
 using Android.Util;
 using Android.Views;
 using Assessment3Hangman.Data;
+using Android.Content;
 
 namespace Assessment3Hangman
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        Button playButton;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.Main);
-
+            playButton = FindViewById<Button>(Resource.Id.btnplay);
+            playButton.Click += PlayButton_Click;
             //if the DB is not there copy it to the Assets folder place
             try
             {
@@ -30,23 +33,20 @@ namespace Assessment3Hangman
             }
         }
 
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(Game))
+                   .SetFlags(ActivityFlags.ReorderToFront);
+            StartActivity(intent);
+        }
+
         public override bool OnCreateOptionsMenu(IMenu menu)
         {
-            menu.Add("Play");
             return base.OnPrepareOptionsMenu(menu);
         }
 
         public override bool OnOptionsItemSelected(IMenuItem item)
         {
-            var itemTitle = item.TitleFormatted.ToString();
-
-            switch (itemTitle)
-            {
-                case "Play":
-                    StartActivity(typeof(Game));
-                    break;
-            }
-
             return base.OnOptionsItemSelected(item);
         }
     }
